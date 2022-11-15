945dc894a58b1685b3d4eefc22b9c55c8728f646
xdmat <xdmateo90@gmail.com>
2022-11-15 14:01:14 -0500

    styles de la pagina

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Manrope", "sans-serif"],
+      },
+      textColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "hsl(217, 19%, 35%)",
+
+        secondary: "hsl(214, 17%, 51%)",
+
+        terceary: "hsl(212, 23%, 69%)",
+
+        cuarto: "hsl(210, 46%, 95%)",
+
+        quinto: "rgba(20,21,25,255)",
+      }),
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+
+        primary: "hsl(217, 19%, 35%)",
+
+        secondary: "hsl(214, 17%, 51%)",
+
+        terceary: "hsl(212, 23%, 69%)",
+
+        cuarto: "hsl(210, 46%, 95%)",
+
+        quinto: "rgba(20,21,25,255)",
+      }),
+    },
   },
   plugins: [],
 };
